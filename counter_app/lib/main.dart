8f238806addf7c5624cd85d24f4ba17108fe594a
TheCre8tor <cre8tor.alexander@gmail.com>
2022-11-25 17:11:42 +0100

    Clear

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CounterPage(title: 'Flutter Demo Home Page'),
+      home: const CounterPage(title: 'Flutter Demo Home Page '),
     );
   }
 }
